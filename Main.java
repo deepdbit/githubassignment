c14925311969c7a509641e9941b4ae67292d072a
SehraSunanda <sunanda.manchanda@gmail.com>
2019-09-13 10:18:09 +0530

    B second commit

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
 	{
 		System.out.println("Tiktok_TestAutomation_script_V1");
 		System.out.println("Tiktok_TestAutomation_script_B_first_commit");
+		System.out.println("Tiktok_TestAutomation_script_B_second_push");
 
 	}
 
