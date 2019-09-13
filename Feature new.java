dd015091bfb7896698d09aedb0e70adaa9e98c07
sgoel <shagungoyal1996@gmail.com>
2019-09-13 10:48:42 +0530

    C feature new

---
+++ Feature new.java
@@ -0,0 +1,11 @@
+package ibagga_delete_26thAug_aid;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		
+                System.out.println("Tiktok_TestAutomation_merge C");
+	}
+
+}
