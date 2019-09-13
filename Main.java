759a2a9182478688b3c7c786d1c5ea17cfde69a8
SehraSunanda <sunanda.manchanda@gmail.com>
2019-09-13 10:15:02 +0530

    B first commit

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		System.out.println("Tiktok_TestAutomation_script_V1");
+		System.out.println("Tiktok_TestAutomation_script_B_first_commit");
+
 	}
 
 }
